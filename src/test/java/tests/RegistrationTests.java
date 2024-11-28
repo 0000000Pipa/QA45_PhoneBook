3907c414c11d4d15c4b8541217e7fb5d901c58bf
margaritacernickaa <rita.chernitskaya@gmail.com>
2024-11-28 20:35:33 +0200

    New project

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,16 +1,22 @@
 package tests;
 
 import manager.ApplicationManager;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginRegPage;
 
+import java.util.Random;
+
 public class RegistrationTests extends ApplicationManager {
+
     @Test
     public void registrationPositiveTest(){
-
+        int i = new Random().nextInt(1000);
         new HomePage(getDriver()).clickBtnLoginHeader();
-        new LoginRegPage(getDriver()).typeLoginForm("email", "password");
+        new LoginRegPage(getDriver()).typeRegistrationForm("frodo_baggins_" + i + "@gmail.com",
+                "Password123!");
+
 
 
 
